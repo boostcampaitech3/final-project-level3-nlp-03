012f93bfc35367c119e48c8c901b42d5408a8307
kimcando <kimcando94@gmail.com>
2022-05-24 15:52:56 +0000

    feat: NAVER 사전으로부터 의미 크롤링

--- data_collection/crawling.py
+++ data_collection/crawling.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
 
 def get_sub(word):
     url = f'https://dict.naver.com/search.dict?dicQuery={word}&query={word}&target=dic&ie=utf8&query_utf=&isOnlyViewEE='
@@ -43,18 +45,18 @@ replace_list = [('\t',''), ('\r',''),('\t',''), ('\n','')]
 
 # noun first
 base_path = './WAI-para_'
-path_lists = ['NNP']
+path_lists = ['NNG','NNP']
 import os, sys
 for path in path_lists:
     name = base_path+path + '.csv'
     nouns = pd.read_csv(name).drop(columns=['Unnamed: 0'])
 
-    from tqdm import tqdm
-    import time
-    data_dict = {'v1':[]}
+    data_dict = {'word':[], 'type':[],'v1':[]}
     for w_idx in tqdm(range(len(nouns[path]))):
         # print(nouns.NNG[w_idx])
         out = get_sub(nouns[path][w_idx])
+        data_dict['word'].append(nouns[path][w_idx])
+        data_dict['word'].append(path)
         if out is None:
             out = ['NA']
             print(w_idx, nouns[path][w_idx], out[0])
@@ -63,7 +65,7 @@ for path in path_lists:
         except:
             data_dict['v1'].append('Not noun')
     df = pd.DataFrame(data_dict)
-    df.to_csv(f'./crawling_{path}')
+    df.to_csv(f'./crawling_{path}.csv')
 
 path_lists = ['VA', 'VV']
 for path in path_lists:
@@ -74,6 +76,9 @@ for path in path_lists:
     for w_idx in tqdm(range(len(verb[path]))):
         # print(nouns.NNG[w_idx])
         out = get_verb(verb[path][w_idx]) #한번에..
+        data_dict['word'].append(verb[path][w_idx])
+        data_dict['word'].append(path)
+
         if out is None:
             out = ['NA']
             print(w_idx, verb[path][w_idx], out[1])
@@ -84,7 +89,7 @@ for path in path_lists:
             data_dict['v1'].append('Not verb')
     df = pd.DataFrame(data_dict)
     name = path.split('/')[1]
-    df.to_csv(f'./crawling_{path}')
+    df.to_csv(f'./crawling_{path}.csv')
 
     
 
