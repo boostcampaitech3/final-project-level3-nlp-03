deef18f274fc6dccad9e040b78ed636fa0e9be18
xuio-0528 <sus5467@naver.com>
2022-05-26 01:39:54 +0000

    Edit : save_steps, eval_steps, evaluation_strategy arguments 추가
    base_config도 숮어을 하기는 했으나
    arguments에서 수정하는 것이 최종적으로
    들어가게 되어 잇다

--- modeling/arguments.py
+++ modeling/arguments.py
@@ -20,13 +20,17 @@ def get_args():
     parser.add_argument("--model_save", type=str, default='./results', help="output dir")
     parser.add_argument("--loggig_steps", type=int, default=500, help="logging steps")
     parser.add_argument("--save_total_limit", type=int, default=2, help="saving limit")
+    parser.add_argument("--save_steps", type=int, default=200, help="saving steps")
+    parser.add_argument("--eval_steps", type=int, default=200, help="evaluation steps")
+    parser.add_argument("--evaluation_strategy", type=str, default='epoch', help="evaluation strategy")
 
 
-    parser.add_argument("--num_train_epochs", type=int, default=6, help="training epochs")
+
+    parser.add_argument("--num_train_epochs", type=int, default=10, help="training epochs")
     parser.add_argument("--train_bs", type=int, default=64, help="batch size per device during training")
     parser.add_argument("--eval_bs", type=int, default=64, help="batch size per device during training")
 
-    parser.add_argument("--lr", type=float, default=5e-5, help="learning rate (default:5e-5)")
+    parser.add_argument("--lr", type=float, default=2e-5, help="learning rate (default:5e-5)")
     args = parser.parse_args()
     return args
 
