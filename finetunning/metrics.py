0624624555b9bca2c6145dbfaa96c076cfc8bc5e
kimcando <kimcando94@gmail.com>
2022-06-01 19:05:11 +0000

    feat: regression, binary 분리, pearson, shear 추가

--- finetunning/metrics.py
+++ finetunning/metrics.py
@@ -3,4 +3,8 @@ from sklearn.metrics import mean_squared_error
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     mse = mean_squared_error(labels, predictions, squared=False)
+
+    # pearson
+    # shear
+    # bin_acc
     return {"mse": mse}
